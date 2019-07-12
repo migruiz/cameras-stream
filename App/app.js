1322d7b737c987b05a62b1b8fab967eab2ae763d
miguel <mig.ruiz@gmail.com>
2019-07-12 13:26:33 +0100

    testing second stream

--- App/app.js
+++ App/app.js
@@ -1,9 +1,26 @@
 
+const { Observable} = require('rxjs');
 const { mergeMap} = require('rxjs/operators');
 const { videoFileStream} = require('./ffmpegVideoExtractor.js');
-
+var Inotify = require('inotify').Inotify;
+var inotify = new Inotify();
 
 
 const  videoExtractorStream = videoFileStream.pipe(mergeMap(v => videoExtractorStream))
 videoExtractorStream.subscribe();
 
+
+
+
+
+const videosFolder = '/videos/'
+
+const videoFilesStream = new Observable(subscriber => {  
+    
+    inotify.addWatch({
+        path: videosFolder,
+        watch_for: Inotify.IN_ALL_EVENTS,
+        callback: event => subscriber.next(event)
+    });
+})
+videoFilesStream.subscribe(v => console.log(v.mask))
