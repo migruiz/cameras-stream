9d43da19d2628eabc29bb6ee0db1f30aee58d460
miguel <mig.ruiz@gmail.com>
2019-07-12 16:53:30 +0100

    buffering readings

--- App/app.js
+++ App/app.js
@@ -1,6 +1,5 @@
-
-const { Observable} = require('rxjs');
-const { mergeMap,filter,map,scan} = require('rxjs/operators');
+const { Observable,of,interval} = require('rxjs');
+const { map,buffer,withLatestFrom} = require('rxjs/operators');
 const { videoFileStream} = require('./ffmpegVideoExtractor.js');
 const { videoSegmentStream } = require('./videoSegmentExtractor');
 const { sensorsReadingStream } = require('./sensorsStreamExtractor');
@@ -9,5 +8,18 @@ global.sensorReadingTopic = 'sensorReadingTopic';
 global.mtqqLocalPath = process.env.MQTTLOCAL;
 
 videoFileStream.subscribe();
-videoSegmentStream.subscribe(v => console.log(v))
-sensorsReadingStream.subscribe(reading => console.log(JSON.stringify(reading)));   
+//videoSegmentStream.subscribe(v => console.log(v))
+//sensorsReadingStream.subscribe(reading => console.log(JSON.stringify(reading)));   
+
+
+
+
+var bufferedSensorReadings=sensorsReadingStream.pipe(buffer(videoSegmentStream));
+    
+var combinedStream = bufferedSensorReadings.pipe(
+    withLatestFrom(videoSegmentStream),
+    map(([first, second]) => {
+        return `First Source (5s): ${JSON.stringify(first)} Second Source (1s): ${JSON.stringify(second)}`;
+    }) 
+)
+combinedStream.subscribe(v => console.log(v));
