8f967cb7e78ee77acbce1df15f75899243226e33
miguel <mig.ruiz@gmail.com>
2019-07-12 13:18:05 +0100

    refactoring extractor

--- App/app.js
+++ App/app.js
@@ -1,51 +1,7 @@
-'use strict';
-var spawn = require('child_process').spawn;
-//var Inotify = require('inotify').Inotify;
-var mqtt = require('./mqttCluster.js');
-//var inotify = new Inotify();
-const { Observable,of,interval } = require('rxjs');
-const { throttleTime,map,groupBy,mergeMap,scan,filter,delay,startWith,tap } = require('rxjs/operators');
-var videosFolder = '/videos/'
-var ffmpegFolder = '/ffmpeg/';
 
+const { mergeMap} = require('rxjs/operators');
+const { videoFileStream} = require('./ffmpegVideoExtractor.js');
 
-
-const videoFileStream = new Observable(subscriber => {  
-        var ffmpegChild = spawn(ffmpegFolder+'ffmpeg'
-        , [
-            '-loglevel'
-            , 'panic'
-            , '-i'
-            , process.env.ENTRANCECAMRTSP
-            , '-pix_fmt'
-            , '+'
-            , '-c:v'
-            , 'copy'
-            , '-c:a'
-            , 'aac'
-            , '-strict'
-            , 'experimental'
-            , '-f'
-            , 'segment'
-            , '-strftime'
-            , '1'
-            , '-segment_time'
-            , '30'
-            , '-segment_format'
-            , 'mp4'
-            , videosFolder + '%s.mp4'
-        ]);
-        console.log("extracting files");
-        ffmpegChild.stdout.on('data', (data) => {
-            console.log(data);
-        });
-        ffmpegChild.stderr.on('data', (data) => {
-            console.error(`child stderr:\n${data}`);
-        });
-        ffmpegChild.on('exit', function (code, signal) {
-            subscriber.complete()
-        });
-});
 
 
 const  videoExtractorStream = videoFileStream.pipe(mergeMap(v => videoExtractorStream))
---
+++ App/ffmpegVideoExtractor.js
@@ -0,0 +1,46 @@
+'use strict';
+var spawn = require('child_process').spawn;
+const { Observable} = require('rxjs');
+
+const videosFolder = '/videos/'
+const ffmpegFolder = '/ffmpeg/';
+
+const videoFileStream = new Observable(subscriber => {  
+    var ffmpegChild = spawn(ffmpegFolder+'ffmpeg'
+    , [
+        '-loglevel'
+        , 'panic'
+        , '-i'
+        , process.env.ENTRANCECAMRTSP
+        , '-pix_fmt'
+        , '+'
+        , '-c:v'
+        , 'copy'
+        , '-c:a'
+        , 'aac'
+        , '-strict'
+        , 'experimental'
+        , '-f'
+        , 'segment'
+        , '-strftime'
+        , '1'
+        , '-segment_time'
+        , '30'
+        , '-segment_format'
+        , 'mp4'
+        , videosFolder + '%s.mp4'
+    ]);
+    console.log("extracting files");
+    ffmpegChild.stdout.on('data', (data) => {
+        console.log(data);
+    });
+    ffmpegChild.stderr.on('data', (data) => {
+        console.error(`child stderr:\n${data}`);
+    });
+    ffmpegChild.on('exit', function (code, signal) {
+        subscriber.complete()
+    });
+});
+
+
+exports.videoFileStream = videoFileStream
