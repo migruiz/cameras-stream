e82c7849b6aadea3cdbcb177153fdcda8d01d5d8
miguel <mig.ruiz@gmail.com>
2019-07-16 13:58:28 +0100

    emailSender.js reacgtive

---
+++ App/emailSender.js
@@ -0,0 +1,75 @@
+const { from,of,Observable,forkJoin } = require('rxjs');
+const { groupBy,mergeMap,throttleTime,map,share,filter,first,mapTo,timeoutWith,toArray,takeWhile,delay,tap} = require('rxjs/operators');
+const fs = require('fs');
+const util = require('util');
+const {google} = require('googleapis');
+
+
+
+const fileStream = path =>  from(util.promisify(fs.readFile)(path));
+const sendEmailStream =(auth,base64EncodedEmail) => from(google.gmail('v1').users.messages.send({
+    auth: auth,
+    userId: 'me',
+    resource: {
+      raw: base64EncodedEmail
+    }
+  }));
+
+const oAuthGoogle = fileStream('c:\\secrets\\credentialsCam.json').pipe
+(
+    map(cr => JSON.parse(cr)),
+    map(cr => new google.auth.OAuth2(cr.installed.client_id, cr.installed.client_secret, cr.installed.redirect_uris[0])),
+)
+const oUathToken = fileStream('c:\\secrets\\tokenCam.json').pipe
+(
+    map(cr => JSON.parse(cr))
+)
+
+
+
+of({}).pipe(
+    map(event => ({event})),
+    map(v => Object.assign({emailParams:getEmailParameters(v.event)}, v)),
+    map(v => Object.assign({base64Email:getBase64Email(v.emailParams)}, v)),
+    mergeMap(v => oAuthGoogle.pipe(map(oUth => Object.assign({oUth}, v)))),
+    mergeMap(v => oUathToken.pipe(map(token => Object.assign({token}, v)))),
+    tap(v => v.oUth.setCredentials(v.token)),
+    mergeMap(v => sendEmailStream(v.oUth,v.base64Email)),
+)
+.subscribe(v=> console.log(v))
+
+
+function getEmailParameters(eventInfo) {
+    const body = 'youtube URL HERE'
+    const htmlBody = body.split('\n').join('\n<br>\n');
+    const emailParams = {
+      fromName: 'home entrance',
+      fromAddress: 'entrance@gmail.com',
+      to: 'mig.ruiz@gmail.com',
+      subject: "sub",
+      body: htmlBody
+    };
+    return emailParams;
+  }
+
+  function getBase64Email(emailParams){
+
+    var email_lines = [];
+
+    email_lines.push('From: "'+ emailParams.fromName + '" <' + emailParams.fromAddress + '>');
+    email_lines.push('To: '+ emailParams.to);
+    email_lines.push('Content-type: text/html;charset=iso-8859-1');
+    email_lines.push('MIME-Version: 1.0');
+    email_lines.push('Subject: ' + emailParams.subject);
+    email_lines.push('');
+    email_lines.push(emailParams.body);
+
+    var email = email_lines.join('\r\n').trim();
+
+    var base64EncodedEmail = new Buffer(email).toString('base64');
+    base64EncodedEmail = base64EncodedEmail.replace(/\+/g, '-').replace(/\//g, '_');
+    return base64EncodedEmail;
+  }
+
+
+exports.emailStream = oAuthGoogle;
