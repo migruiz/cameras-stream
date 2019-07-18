6f656d88ca11b78466b8c3b481c1e2eeeda3893a
miguel <mig.ruiz@gmail.com>
2019-07-18 14:42:21 +0100

    extracting common lob

---
+++ App/googleOauth.js
@@ -0,0 +1,68 @@
+'use strict';
+
+/**
+ * Usage: node upload.js PATH_TO_VIDEO_FILE
+ */
+
+const fs = require('fs');
+const util = require('util');
+const readline = require('readline');
+const { from,of,Observable,forkJoin,iif,throwError,defer } = require('rxjs');
+const { groupBy,mergeMap,throttleTime,map,share,filter,first,mapTo,timeoutWith,toArray,takeWhile,delay,tap,catchError,concatMap} = require('rxjs/operators');
+
+const {google} = require('googleapis');
+
+
+const fileStream = path =>  from(util.promisify(fs.readFile)(path));
+const readDirStream = path =>  from(util.promisify(fs.readdir)(path));
+
+const oAuthGoogle = fileName => fileStream(fileName).pipe
+(
+    map(cr => JSON.parse(cr)),
+    map(cr => new google.auth.OAuth2(cr.installed.client_id, cr.installed.client_secret, cr.installed.redirect_uris[0])),
+)
+const oUathToken = fileName =>  fileStream(fileName).pipe
+(
+    map(cr => JSON.parse(cr))
+)
+
+
+
+
+
+
+const oauthStream = (authInfo) => oAuthGoogle(authInfo.credential).pipe(
+    map(oUth => ({oUth})),
+    mergeMap(v => oUathToken(authInfo.token).pipe(map(token => Object.assign({token}, v)))),
+    tap(v => v.oUth.setCredentials(v.token)),
+    map(v => v.oUth) 
+)
+
+
+
+const executeRetryingStream = (projects,index,oAuthProcess) =>
+
+
+oauthStream(projects[index])
+  .pipe(  
+    concatMap(oAuth => oAuthProcess(oAuth)),
+    catchError(err => iif(() => index < projects.length - 1 && err.code===403,  defer(() => executeRetryingStream(projects,index+1,oAuthProcess)), throwError(err) ) )
+    )
+
+const credesDir = '/repos/secrets/cameras/secrets'
+const readDirsStream = 
+readDirStream(credesDir).pipe(
+  concatMap(v => v),
+  map(v => ({
+    credential: `${credesDir}/${v}/credentialsCam.json`,
+    token: `${credesDir}/${v}/tokenCam.json`
+  })),
+  tap(v=>console.log(v)),
+  toArray()
+)
+
+const resultStream = oAuthProcess => readDirsStream.pipe(concatMap(arr=> executeRetryingStream(arr,0,oAuthProcess)))
+
+
+
+exports.oauthStream = resultStream
--- App/uploadYoutube.js
+++ App/uploadYoutube.js
@@ -9,12 +9,11 @@ const util = require('util');
 const readline = require('readline');
 const { from,of,Observable,forkJoin,iif,throwError,defer } = require('rxjs');
 const { groupBy,mergeMap,throttleTime,map,share,filter,first,mapTo,timeoutWith,toArray,takeWhile,delay,tap,catchError,concatMap} = require('rxjs/operators');
-
+const { oauthStream } = require('./googleOauth');
 const {google} = require('googleapis');
 
 
-const fileStream = path =>  from(util.promisify(fs.readFile)(path));
-const readDirStream = path =>  from(util.promisify(fs.readdir)(path));
+
 const uploadVideoStream =(auth,fileName) => from(
 
     google.youtube({
@@ -43,48 +42,10 @@ const uploadVideoStream =(auth,fileName) => from(
 
 
 
-const oAuthGoogle = fileName => fileStream(fileName).pipe
-(
-    map(cr => JSON.parse(cr)),
-    map(cr => new google.auth.OAuth2(cr.installed.client_id, cr.installed.client_secret, cr.installed.redirect_uris[0])),
-)
-const oUathToken = fileName =>  fileStream(fileName).pipe
-(
-    map(cr => JSON.parse(cr))
-)
 
-
-
-
-
-
-const uploadCompleteStream = (authInfo,videofileName) => oAuthGoogle(authInfo.credential).pipe(
-    map(oUth => ({oUth})),
-    mergeMap(v => oUathToken(authInfo.token).pipe(map(token => Object.assign({token}, v)))),
-    tap(v => v.oUth.setCredentials(v.token)),
-    mergeMap(v => uploadVideoStream(v.oUth,videofileName))    
-)
-
-
-
-const uploadYoutubeQuotaStream = (projects,index,videofileName) => uploadCompleteStream(projects[index],videofileName)
-  .pipe(  
-    catchError(err => iif(() => index < projects.length - 1 && err.code===403,  defer(() => uploadYoutubeQuotaStream(projects,index+1,videofileName)), throwError(err) ) )
-    )
-
-const credesDir = '/secrets/yt'
-const readDirsStream = 
-readDirStream(credesDir).pipe(
-  concatMap(v => v),
-  map(v => ({
-    credential: `${credesDir}/${v}/credentialsCam.json`,
-    token: `${credesDir}/${v}/tokenCam.json`
-  })),
-  tap(v=>console.log(v)),
-  toArray()
-)
-
-const resultStream = videofileName => readDirsStream.pipe(concatMap(arr=> uploadYoutubeQuotaStream(arr,0,videofileName)))
+const resultStream =function(fileName){
+  return oauthStream(auth => uploadVideoStream(auth,fileName))
+}
 
 
 
