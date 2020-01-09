b6b0213a68f1f71eaaf696487034d43cd92a83b0
Miguel Ruiz <mig.ruiz@gmail.com>
2020-01-09 17:31:44 +0000

    rem oving consolo.log

--- App/movementStreamExtractor.js
+++ App/movementStreamExtractor.js
@@ -65,11 +65,9 @@ var movementSharedStream = movementSensorsReadingStream.pipe(
     )
     ,filter( e =>e.length>0)
     ,map(a=> ({startedAt:a[0],endedAt:a[a.length-1]}))
-    ,tap(c => console.log(JSON.stringify(c)))
     ,mergeMap( ev =>         
         segmentStream.pipe(
-            tap(c => console.log(JSON.stringify(c)))
-            ,filter(s=>s.endTime > ev.startedAt &&  s.startTime < ev.endedAt )
+            filter(s=>s.endTime > ev.startedAt &&  s.startTime < ev.endedAt )
             ,toArray()
             ,map(a => Object.assign({videos:a},ev))
         )
