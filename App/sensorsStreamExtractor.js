b0abd051d060db9f13726ac3cb3be8d2477ac021
Miguel Ruiz <mig.ruiz@gmail.com>
2020-02-06 15:15:39 +0000

    counting events

--- App/sensorsStreamExtractor.js
+++ App/sensorsStreamExtractor.js
@@ -1,5 +1,5 @@
-const { Observable,of,merge,empty } = require('rxjs');
-const { groupBy,mergeMap,throttleTime,map,reduce, share,shareReplay, filter,first,mapTo,timeoutWith,toArray,takeWhile,delay,tap,distinct} = require('rxjs/operators');
+const { Observable,of,merge,empty,timer } = require('rxjs');
+const { groupBy,mergeMap,throttleTime,map,reduce,takeUntil, share,shareReplay, filter,first,mapTo,timeoutWith,toArray,takeWhile,delay,tap,distinct} = require('rxjs/operators');
 var mqtt = require('./mqttCluster.js');
 const VIDEOSEGMENTLENGTH=30*1000;
 const WAITTIMEFORMOVEMENTAFTEROPENINGDOOR=15*1000;
@@ -11,36 +11,68 @@ const sensorsReadingStream = new Observable(async subscriber => {
     console.log('subscribing sensorsReadingStream')
     var mqttCluster=await mqtt.getClusterAsync()   
     mqttCluster.subscribeData('test', function(content){
-        if (content.ID==='door'){
-            subscriber.next(content)
+        if (content.ID==='move'){
+            subscriber.next({data:'16340250'})
         }
     });
     mqttCluster.subscribeData('test', function(content){
-        if (content.ID==='move'){
-            subscriber.next(content)
+        if (content.ID==='door'){
+            subscriber.next({data:'233945'})
         }
     });
 });
 
-const sharedStream = sensorsReadingStream.pipe(share())
+const ESPDELAY = 3000
+
+const sharedStream = sensorsReadingStream.pipe(
+        map(r => ({
+                sensorId: parseInt(r.data),
+                timestamp: (new Date).getTime(),                
+            })),
+        share()
+    )
 
 
-const doorOpenSensor = sharedStream.pipe(filter(r => r.ID==='door'),share());
-const outsideMovementSensor = sharedStream.pipe(filter(r => r.ID==='move'),share());
-const lastEmitions = outsideMovementSensor.pipe(shareReplay(undefined,10000,undefined))
-lastEmitions.subscribe()
+const doorOpenSensor = sharedStream.pipe(
+    filter(r => r.sensorId===233945),
+    throttleTime(5000),
+    map(r=>({sensorId:r.sensorId, timestamp:r.timestamp - ESPDELAY})),
+    share()
+    );
+const outsideMovementSensor = sharedStream.pipe(filter(r => r.sensorId===16340250),share());
+
+const movementBeforeDoorEvent = outsideMovementSensor.pipe(shareReplay(undefined,20000 + ESPDELAY,undefined))
+const movementAfterDoorEvent = outsideMovementSensor.pipe(shareReplay(undefined,ESPDELAY,undefined))
+movementBeforeDoorEvent.subscribe()
+movementAfterDoorEvent.subscribe()
 
 
-const bef = doorOpenSensor.pipe(
-    
-    mergeMap(d => lastEmitions.pipe(        
-        timeoutWith(0,empty()),        
+const beforeDoorEventStream = doorOpenSensor.pipe(
+    mergeMap(d => movementBeforeDoorEvent.pipe(        
+        timeoutWith(0,empty()),   
+        takeWhile(s=> s.timestamp < d.timestamp),
         reduce((acc, _ ) => acc + 1, 0),
-        map(n => Object.assign({count:n}, d))
+        map(n => Object.assign({countBefore:n,finished:false}, d))
         )
     )
 )
-bef.subscribe(c=> console.log(c))
+const afterDoorEventStream = doorOpenSensor.pipe(
+    
+    mergeMap(d => movementAfterDoorEvent.pipe(     
+        takeUntil(timer(20000 - ESPDELAY)),        
+        reduce((acc, _ ) => acc + 1, 0),
+        map(n => Object.assign({countAfter:n,finished:true}, d))
+        )
+    )
+)
+
+const result = merge(beforeDoorEventStream,afterDoorEventStream).pipe(
+    groupBy(r => r.timestamp, r => r),
+    mergeMap(stream => stream.pipe( takeWhile(e => !e.finished,true),toArray())),
+    map(([befDoor,afterDoor]) =>  Object.assign(befDoor, afterDoor))
+)
+result.subscribe(c=> console.log(c))
+
 return
 
 const movementBeforeOpeningDoorStream = outsideMovementSensor.pipe(
