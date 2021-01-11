07422a31376417119504fb2319e53b3391104cc8
Miguel <mig.ruiz@gmail.com>
2021-01-11 12:07:47 +0000

    joining event fie

--- App/movement/movementStreamExtractor.js
+++ App/movement/movementStreamExtractor.js
@@ -72,13 +72,131 @@ segmentStream.subscribe()
  var streamToListen =   combinedStream.pipe(
     mergeMap( ev =>         
         segmentStream.pipe(
-            filter(s=>s.endTime > ev.startVideoAt)
-            ,takeWhile(s=> s.startTime < ev.endVideoAt )
+            filter(s=>s.endTime > ev.start)
+            ,takeWhile(s=> s.startTime < ev.end)
             ,toArray()
             ,map(a => Object.assign({videos:a, videosStartTime:a[0].startTime, videosEndTime:a[a.length-1].endTime},ev))
+            ,map(event => Object.assign({videosStartTimeSecs:Math.round(parseFloat(event.videosStartTime/1000)),videosEndTimeSecs:Math.round(parseFloat(event.videosEndTime/1000))}))
+            ,map(event => Object.assign({startSecs:Math.round(parseFloat(event.start/1000)),endSecs:Math.round(parseFloat(event.end/1000))}))
         )
     )
 )
+
+
+const extractVideoStream = streamToListen.pipe(
+    map(event => Object.assign({filesToJoinPath:`${videosFolderTemp}${event.start}.txt`}, event)),
+    map(event => Object.assign({joinedVideoPath:`${videosFolderTemp}${event.start}_joined.mp4`}, event)),
+    map(event => Object.assign({targetVideoPath:`${videosFolderTemp}${event.start}.mp4`}, event)),
+    map(event => Object.assign({filesToJoinContent:event.videos.map(v => `file ${v}`).join('\r\n')}, event)),
+    mergeMap(v => writeFileStream(v.filesToJoinPath,v.filesToJoinContent).pipe(endWith(v))),    
+    mergeMap(v => joinFilesStream(v.filesToJoinPath,v.joinedVideoPath).pipe(endWith(v))),
+    mergeMap(v => ffmpegextractVideoStream(v.start - v.videosStartTimeSecs, v.end - v.start, v.joinedVideoPath,v.targetVideoPath).pipe(endWith(v))),
+    mergeMap(v => removeFile(v.filesToJoinPath).pipe(endWith(v))),
+    mergeMap(v => removeFile(v.joinedVideoPath).pipe(endWith(v))),
+);
+
+
+const removeFile = path =>  from(util.promisify(fs.unlink)(path)).pipe(switchMapTo(empty()));
+
+const writeFileStream = (path,content) =>  Observable.create(subscriber => {  
+    fs.writeFile(path, content, function (err) {
+        if (err) {
+            subscriber.error(err)
+        }
+        subscriber.complete();
+    });
+});
+
+
+
+
+
+
+const joinFilesStream = (filesToJoinPath,targetFile) => Observable.create(subscriber => {   
+    const params=  [
+        '-y'
+        , '-f'
+        , 'concat'
+        , '-safe'
+        , '0'
+        , '-i'
+        , filesToJoinPath
+        , '-c'
+        , 'copy'
+        , targetFile
+    ]
+    const ffmpegChild = spawn(ffmpegFolder+'ffmpeg',params);
+    var result = '';
+    ffmpegChild.stdout.on('data', (data) => {
+        result += data.toString();
+    });
+    var errorResult = '';
+    ffmpegChild.stderr.on('data', (data) => {
+      errorResult += data.toString();
+    });
+    ffmpegChild.on('exit', function (code, signal) {
+      if (code) {
+        console.log(JSON.stringify({filesToJoinPath,targetFile,code,signal,result,errorResult}))
+        subscriber.error('joinFilesStream error');
+      } else if (signal) {
+        console.log(JSON.stringify({filesToJoinPath,targetFile,code,signal,result,errorResult}))
+        subscriber.error('joinFilesStream error');
+      } else {
+        subscriber.complete();
+      }     
+    }); 
+});
+
+
+
+function toHHMMSS(sec_num) {
+  var hours   = Math.floor(sec_num / 3600);
+  var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+  var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+  if (hours   < 10) {hours   = "0"+hours;}
+  if (minutes < 10) {minutes = "0"+minutes;}
+  if (seconds < 10) {seconds = "0"+seconds;}
+  return hours+':'+minutes+':'+seconds;
+}
+
+const ffmpegextractVideoStream = (startPosition,lengthSecs,joinedVideoPath,targetVideoPath) => Observable.create(subscriber => {   
+    const params = [
+        '-y'
+        , '-ss'
+        , toHHMMSS(startPosition)
+        , '-i'
+        , joinedVideoPath
+        , '-t'
+        , toHHMMSS(lengthSecs)
+        , '-vcodec'
+        , 'copy'
+        , '-acodec'
+        , 'copy'
+        , targetVideoPath
+    ];
+    const ffmpegChild = spawn(ffmpegFolder+'ffmpeg',params);
+    var result = '';
+    ffmpegChild.stdout.on('data', (data) => {
+        result += data.toString();
+    });
+    var errorResult = '';
+    ffmpegChild.stderr.on('data', (data) => {
+      errorResult += data.toString();
+    });
+    ffmpegChild.on('exit', function (code, signal) {
+      if (code) {
+        console.log(JSON.stringify({startPosition,joinedVideoPath,targetVideoPath,code,signal,result,errorResult}))
+        subscriber.error('ffmpegextractVideoStream error');
+      } else if (signal) {
+        console.log(JSON.stringify({startPosition,joinedVideoPath,targetVideoPath,code,signal,result,errorResult}))
+        subscriber.error('ffmpegextractVideoStream error');
+      } else {
+        subscriber.complete();
+      }     
+    }); 
+});
+
 
 
 
