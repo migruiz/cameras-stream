7f957e19e12c02fa63bb0db47ac8ab166a58267c
Miguel <mig.ruiz@gmail.com>
2021-01-11 11:26:32 +0000

    getting videos to use

--- App/movement/movementStreamExtractor.js
+++ App/movement/movementStreamExtractor.js
@@ -32,7 +32,57 @@ const combinedStream = turnOnStream.pipe(
     )),
 )
 
-exports.movementStream = combinedStream
+
+
+const { probeVideoInfo} = require('../ffprobeVideoDetailsExtractor');
+const path = require('path');
+var Inotify = require('inotify').Inotify;
+var inotify = new Inotify();
+
+
+
+const videosFolder = '/videos/'
+
+const videoFilesStream = new Observable(subscriber => {  
+    inotify.addWatch({
+        path: videosFolder,
+        watch_for: Inotify.IN_ALL_EVENTS,
+        callback: event => subscriber.next(event)
+    });
+})
+const segmentStream = videoFilesStream.pipe(
+    filter(e => e.mask & Inotify.IN_CLOSE_WRITE),
+    timeout(1 * 60 * 1000),
+    map(e => e.name),
+    mergeMap(fileName => probeVideoInfo(videosFolder + fileName)),
+    mergeMap(videoInfo => videoInfo.format.duration < 20 ? throwError('Error length video '+ JSON.stringify(videoInfo)) : of(videoInfo)),
+    map(videoInfo => (
+        {
+            fileName:videoInfo.format.filename,
+            startTime:1000 * parseInt(path.basename(videoInfo.format.filename,'.mp4')),            
+            length:1000 * Math.round(parseFloat(videoInfo.format.duration))
+        }
+    )),
+    map(videoInfo => Object.assign({endTime:videoInfo.startTime+videoInfo.length}, videoInfo)),
+    shareReplay(10)
+)
+segmentStream.subscribe()
+
+
+ var streamToListen =   combinedStream.pipe(
+    mergeMap( ev =>         
+        segmentStream.pipe(
+            filter(s=>s.endTime > ev.startVideoAt)
+            ,takeWhile(s=> s.startTime < ev.endVideoAt )
+            ,toArray()
+            ,map(a => Object.assign({videos:a, videosStartTime:a[0].startTime, videosEndTime:a[a.length-1].endTime},ev))
+        )
+    )
+)
+
+
+
+exports.movementStream = streamToListen
 
 
 
